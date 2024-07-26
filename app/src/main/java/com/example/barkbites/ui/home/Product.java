8dcf05093decda96f1bbb318d5b6ee22d015d1a4
Shenal Senaratne <136364310+ShenalSen@users.noreply.github.com>
2024-07-26 12:46:47 +0530

    go

---
+++ app/src/main/java/com/example/barkbites/ui/home/Product.java
@@ -0,0 +1,30 @@
+package com.example.barkbites.ui.home;
+
+public class Product {
+    private int id;
+    private String title;
+    private String description;
+    private double price;
+    private String image;
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
